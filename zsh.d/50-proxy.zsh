62ee0e4c0bdcfa41b449891dcbaf8813701062c4
Cees van de Griend <cees.van.de.griend@protonmail.com>
2023-10-17 11:09:00 +0200

    Fixed: typo

--- zsh.d/50-proxy.zsh
+++ zsh.d/50-proxy.zsh
@@ -1,5 +1,5 @@
 
-if [ $(route get default | grep -q kade3.home) -eq 1 ] ;
+if [ $(route get default | grep -c kade3.home) -eq 1 ] ; then
     export http_proxy=http://proxy01.kade3.home:3128/
     export https_proxy=http://proxy01.kade3.home:3128/
     export ftp_proxy=http://proxy01.kade3.home:3128/
